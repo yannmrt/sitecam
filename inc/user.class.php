33409b0cab214eabec95c70ac77edb22dd3c07e9
yannmrt <70692715+yannmrt@users.noreply.github.com>
2022-01-04 08:44:04 +0100

    Add files via upload

--- inc/user.class.php
+++ inc/user.class.php
@@ -28,8 +28,12 @@ class User {
         $password = hash("sha512", $password);
 
         if(!empty($email) AND !empty($password)) {
-            $req = $this->_sql->prepare("INSERT INTO user SET email = ?, password = ? AND permission = ?");
-            $req->execute(array($email, $password, "0"));
+            $req = $this->_sql->prepare("INSERT INTO user SET email = :email, password = :password, permission = :permission");
+            $req->execute(array(
+                "email" => $email, 
+                "password" => $password,
+                "permission" => "0"
+            ));
 
             header('Location: login.php');
         } else {
