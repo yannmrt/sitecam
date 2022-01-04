bcbf0ddc4715884ae14828eecc5612e51933cf12
yannmrt <70692715+yannmrt@users.noreply.github.com>
2022-01-04 09:44:29 +0100

    Add files via upload

--- inc/header.php
+++ inc/header.php
@@ -6,7 +6,7 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php">Accueil</a></li>
                         <li class="nav-item"><a class="nav-link" href="gestion.php">Gestion</a></li>
-                        <li class="nav-item"><a class="nav-link" href="addcam.php">Ajouter une caméra</a></li>
+                        <li class="nav-item"><a class="nav-link" href="addcam.php">Caméra</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Compte</a>
                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
